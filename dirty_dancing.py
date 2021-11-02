ced62599e5ac2314b8d2581ac4809f1ae0b94521
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2021-11-02 18:15:02 +0100

    Dirty dancing 2.

--- dirty_dancing.py
+++ dirty_dancing.py
@@ -4,7 +4,7 @@ from scipy.interpolate import interp1d
 def ip_stream_generator(X, y,
                         stream_features = 2,
                         n_drifts = 3,
-                        interpolation = 'cubic',
+                        interpolation = 'nearest',
                         random_state = None):
     np.random.seed(random_state)
     n_samples, concept_features = X.shape
@@ -20,8 +20,20 @@ def ip_stream_generator(X, y,
     for d_s in range(stream_features):
         for d_c in range(concept_features):
             original_values = base_projections[:, d_c, d_s]
-            f = interp1d(drift_basepoints, original_values, kind=interpolation)
-            continous_projections[:, d_c, d_s] = f(stream_basepoints)
+            if (original_values.shape[0] <= 3) and (interpolation == 'cubic'):
+                f_l = interp1d(drift_basepoints,
+                               original_values,
+                               kind='linear')
+                u_drift_basepoints = np.linspace(0,n_samples,4).astype(int)
+                u_original_values = f_l(u_drift_basepoints)
+
+                f = interp1d(u_drift_basepoints,
+                             u_original_values,
+                             kind=interpolation)
+                continous_projections[:, d_c, d_s] = f(stream_basepoints)
+            else:
+                f = interp1d(drift_basepoints, original_values, kind=interpolation)
+                continous_projections[:, d_c, d_s] = f(stream_basepoints)
 
     X_s = np.sum(X[:, :, np.newaxis] * continous_projections, axis=1)
 
