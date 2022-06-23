4d3dd9e6c7c0d8649387f8e4b96469c684b858dd
Joana <asiakom24@vp.pl>
2022-06-23 20:34:32 +0200

    improve plot

--- complexity_stream.py
+++ complexity_stream.py
@@ -19,7 +19,11 @@ ds = np.genfromtxt("datasets/wisconsin.csv", delimiter=",")
 X = ds[:, :-1]
 y_ = ds[:, -1].astype(int)
 
-X_s_cubic, y = ip_stream_generator(X, y_, total_samples=50000, stream_features=2, random_state=1209388, n_drifts=n_drfs, interpolation='cubic')
+cc = px.ComplexityCalculator()
+original_complexity = cc.fit(X, y_).complexity
+
+
+X_s_cubic, y = ip_stream_generator(X, y_, total_samples=50000, stream_features=2, random_state=179234, n_drifts=n_drfs, interpolation='cubic')
 
 chunk_size = 250
 chunk_number = int(X_s_cubic.shape[0]/chunk_size)
@@ -66,7 +70,7 @@ for i in range(chunk_number):
     ax[1].plot(np.arange(chunk_number-1) ,mean_chunk1_c, label = 'feature B', color='cornflowerblue')
     ax[1].set_xlabel('chunk number')
     ax[1].set_ylabel('mean feature value')
-    ax[1].vlines(drifts, ymin_plot, ymax_plot, ls=":", color='r')
+    ax[1].vlines(drifts, ymin_plot, ymax_plot, ls=":", color='r', linewidth=0.5)
     ax[1].vlines(i, ymin_plot, ymax_plot, ls="-", color='black')
     ax[1].set_ylim(ymin_plot, ymax_plot)
     ax[1].set_xlim(0, chunk_number)
@@ -81,47 +85,53 @@ for i in range(chunk_number):
     cols = cmap(np.linspace(0,1,5))
     for m_id, m in enumerate(cc.metrics[:5]):
         ax[2].plot(np.arange(chunk_number), medfilt(all_complexity[:,m_id], k), color=cols[m_id], label=m.__name__)
+        ax[2].hlines(original_complexity[m_id], 0, chunk_number, color=cols[m_id], ls=":")
     ax[2].set_title("feature based")
 
     # linearity
     cols = cmap(np.linspace(0,1,3))
     for m_id, m in enumerate(cc.metrics[5:8]):
         ax[3].plot(np.arange(chunk_number), medfilt(all_complexity[:,m_id+5],k), color=cols[m_id], label=m.__name__)
+        ax[3].hlines(original_complexity[m_id+5], 0, chunk_number, color=cols[m_id], ls=":")
     ax[3].set_title("linearity")
 
     # neighborhood
     cols = cmap(np.linspace(0,1,6))
     for m_id, m in enumerate(cc.metrics[8:14]):
         ax[4].plot(np.arange(chunk_number), medfilt(all_complexity[:,m_id+8],k), color=cols[m_id], label=m.__name__)
+        ax[4].hlines(original_complexity[m_id+8], 0, chunk_number, color=cols[m_id], ls=":")
     ax[4].set_title("neighborhood")
 
     # network
     cols = cmap(np.linspace(0,1,3))
     for m_id, m in enumerate(cc.metrics[14:17]):
         ax[5].plot(np.arange(chunk_number), medfilt(all_complexity[:,m_id+14],k), color=cols[m_id], label=m.__name__)
+        ax[5].hlines(original_complexity[m_id+14], 0, chunk_number, color=cols[m_id], ls=":")
     ax[5].set_title("network")
 
     # dimensionality
     cols = cmap(np.linspace(0,1,3))
     for m_id, m in enumerate(cc.metrics[17:20]):
         ax[6].plot(np.arange(chunk_number), medfilt(all_complexity[:,m_id+17],k), color=cols[m_id], label=m.__name__)
+        ax[6].hlines(original_complexity[m_id+17], 0, chunk_number, color=cols[m_id], ls=":")
     ax[6].set_title("dinemsionality")
 
     # class imbalance
     cols = cmap(np.linspace(0,1,2))
     for m_id, m in enumerate(cc.metrics[20:]):
         ax[7].plot(np.arange(chunk_number), medfilt(all_complexity[:,m_id+20],k), color=cols[m_id], label=m.__name__)
+        ax[7].hlines(original_complexity[m_id+20], 0, chunk_number, color=cols[m_id], ls=":")
     ax[7].set_title("class imbalance")
 
     for a in range(8):
         ax[a].spines.top.set_visible(False)
         ax[a].spines.right.set_visible(False)
-        ax[a].grid()
+        ax[a].grid(linewidth=0.5, linestyle=":")
     
     for a in range(2,8):
         ax[a].set_ylim(0,1)
         ax[a].legend(loc=2)
-        ax[a].vlines(drifts, 0, 1, ls=":", color='r')
+        ax[a].vlines(drifts, 0, 1, ls=":", color='r', linewidth=0.5)
 
 
     plt.tight_layout()
